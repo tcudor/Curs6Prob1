14ccdf08f68e5f31323fb17c374ca0553725fd28
asus <asus@ASUS>
2023-10-15 22:25:58 +0300

    Add project files.

---
+++ ArrayHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curs6Prob1
+{
+    internal class ArrayHelper
+    {
+        public static int MinR(int[] array, int start, int end)
+        {
+            if (start == end)
+            {
+                return array[start];
+            }
+
+            int middle = (start + end) / 2;
+            int leftMin = MinR(array, start, middle);
+            int rightMin = MinR(array, middle + 1, end);
+
+            return leftMin < rightMin ? leftMin : rightMin;
+        }
+
+        public static int MaxR(int[] array, int start, int end)
+        {
+            if (start == end)
+            {
+                return array[start];
+            }
+
+            int middle = (start + end) / 2;
+            int leftMax = MaxR(array, start, middle);
+            int rightMax = MaxR(array, middle + 1, end);
+
+            return leftMax > rightMax ? leftMax : rightMax;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+
+namespace Curs6Prob1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = ConsoleHelper.ReadNumber("The size of the array = ", 3, 0);
+            int[] array = new int[size];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int element = ConsoleHelper.ReadNumber($"element[{i}] = ", 3, 0);
+                array[i] = element;
+            }
+            ConsoleHelper.PrintArray("Array=", array);
+
+            int minR = ArrayHelper.MinR(array, 0, size-1);
+            Console.WriteLine($"Min = {minR}");
+
+            int maxR = ArrayHelper.MaxR(array, 0, size-1);
+            Console.WriteLine($"Max = {maxR}");
+        }
+    }       
+}
